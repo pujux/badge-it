93a77f6fef4c08e881c2c20645baf90349d21369
Julian Pufler <puf17640@spengergasse.at>
2020-07-15 16:00:35 +0200

    v1

---
+++ models.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose')
+
+module.exports = mongoose.model('Entry', new mongoose.Schema({
+  key: String,
+  counter: Number
+}))
--- routes/index.js
+++
@@ -1,5 +0,0 @@
-const router = require('express').Router()
-
-router.get('/', (req, res, next) => res.render('index'))
-
-module.exports = router
