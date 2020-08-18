f4fd96bd2416ff21ce7882e38e447e2caa704b45
Julian Pufler <puf17640@spengergasse.at>
2020-08-18 16:52:42 +0200

    added created and updated badges and fixed cron schedule

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require('express'),
 	Entry = require('./models'),
 	request = require('@aero/centra'),
 	{ differenceInYears } = require('date-fns'),
-	cron = require('cron')
+	cron = require('cron'),
+	moment = require('moment')
 
 const app = express(),
 	config = require('dotenv').config()
@@ -19,6 +20,8 @@ const githubHeaders = {
 	Authorization: `Basic ${Buffer.from(`${process.env.GITHUB_ID}:${process.env.GITHUB_TOKEN}`, 'utf8').toString('base64')}`,
 	'User-Agent': 'pufler-dev'
 }
+
+const createError = (res, error) => res.status(5e2).send({ message: 'An error occured while processing your request, please create an issue on https://github.com/puf17640/git-badges/issues and give as much detail as you can. Thanks!', error });
 
 app.use(express.urlencoded({ extended: false }))
 app.set('trust proxy', true)
@@ -40,7 +43,7 @@ app.get('/years/:user', async (req, res) => {
 	const { user } = req.params
 	const response = await request(`https://api.github.com/users/${user}`)
 		.header(githubHeaders).json()
-	if (!response.created_at) return res.status(404).send(response)
+	if (!response.created_at) return createError(response.message)
 	const yearsAtGitHub = differenceInYears(Date.now(), Date.parse(response.created_at))
 	res.contentType('image/svg+xml')
 		.header('Cache-Control', 'no-cache,max-age=600')
@@ -51,7 +54,7 @@ app.get('/repos/:user', async (req, res) => {
 	const { user } = req.params
 	const response = await request(`https://api.github.com/users/${user}`)
 		.header(githubHeaders).json()
-	if (!response.public_repos) return res.status(404).send(response)
+	if (!response.public_repos) return createError(response.message)
 	res.contentType('image/svg+xml')
 		.header('Cache-Control', 'no-cache,max-age=600')
 		.send(await request(`https://img.shields.io/badge/Repos-${response.public_repos}-brightgreen${req.originalUrl.slice(req.originalUrl.indexOf('?'))}`).raw())
@@ -61,20 +64,38 @@ app.get('/gists/:user', async (req, res) => {
 	const { user } = req.params;
 	const response = await request(`https://api.github.com/users/${user}/gists`)
 		.header(githubHeaders).json();
-	if (!Array.isArray(response) || response.length === 0) return res.status(404).send(response);
+	if (!Array.isArray(response) || response.length === 0) return createError(response.message)
 	res.contentType('image/svg+xml')
 		.header('Cache-Control', 'no-age', 'max-age=600')
 		.send(await request(`https://img.shields.io/badge/Gists-${response.length}-brightgreen${req.originalUrl.slice(req.originalUrl.indexOf('?'))}`).raw());
 })
 
-app.use((_, res) => res.redirect('https://pufler.dev/git-badges/'))
+app.get('/updated/:user/:repo', async (req, res) => {
+	const { user, repo } = req.params;
+	const response = await request(`https://api.github.com/repos/${user}/${repo}`)
+		.header(githubHeaders).json()
+	if (!response.id) return createError(response.message)
+	res.contentType('image/svg+xml')
+		.header('Cache-Control', 'no-age', 'max-age=600')
+		.send(await request(`https://img.shields.io/badge/Updated-${moment(response.updated_at).fromNow()}-brightgreen${req.originalUrl.slice(req.originalUrl.indexOf('?'))}`).raw());
+})
 
-app.use((err, _, res) => res.status(err.status || 5e2).send({ error: err.message }))
+app.get('/created/:user/:repo', async (req, res, next) => {
+	const { user, repo } = req.params;
+	const response = await request(`https://api.github.com/repos/${user}/${repo}`)
+		.header(githubHeaders).json()
+	if (!response.id) return createError(response.message)
+	res.contentType('image/svg+xml')
+		.header('Cache-Control', 'no-age', 'max-age=600')
+		.send(await request(`https://img.shields.io/badge/Created-${moment(response.created_at).fromNow()}-brightgreen${req.originalUrl.slice(req.originalUrl.indexOf('?'))}`).raw());
+})
 
-app.listen(process.env.PORT, () => console.log(`[INFO]: listening on port ${process.env.PORT}`))
+app.use((_, res) => res.redirect('https://badges.pufler.dev/'))
 
-cron.job('*/30 * * * *', async () => {
+app.listen(process.env.PORT, () => console.log(`[Info]: listening on port ${process.env.PORT}`))
+
+cron.job('*/10 * * * *', async () => {
 	const { resources } = await request(`https://api.github.com/rate_limit`)
 		.header(githubHeaders).json();
 	console.log(`[Rate Limit]: ${resources.core.remaining} / ${resources.core.limit} (${new Date(resources.core.reset*1e3).toLocaleString()})`)
-})
+}).start()
