20164789d78ca2ffa313cddbc5f7f1c035743f00
Julian Pufler <puf17640@spengergasse.at>
2020-09-06 01:03:57 +0200

    added contributors display

--- server.js
+++ server.js
@@ -4,22 +4,22 @@ const express = require('express'),
 	request = require('@aero/centra'),
 	{ differenceInYears } = require('date-fns'),
 	cron = require('cron'),
-	moment = require('moment')
+	moment = require('moment');
 
 const app = express(),
-	config = require('dotenv').config()
+	config = require('dotenv').config();
 
 if (config.error) {
-	console.warn('[WARNING]: cannot parse .env file, falling back to systems ones')
+	console.warn('[WARNING]: cannot parse .env file, falling back to systems ones');
 }
 
-mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/gh-visitors', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/git-badges', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
 const githubHeaders = {
 	Authorization: `Basic ${Buffer.from(`${process.env.GITHUB_ID}:${process.env.GITHUB_TOKEN}`, 'utf8').toString('base64')}`,
 	'User-Agent': 'pufler-dev',
 	Accept: 'application/vnd.github.cloak-preview+json'
-}
+};
 
 const periodicities = {
 	daily: 'day',
@@ -27,92 +27,139 @@ const periodicities = {
 	monthly: 'month',
 	yearly: 'year',
 	all: null
-}
+};
 
-const createError = (res, error) => res.status(5e2).send(
-	{ message: 'An error occured while processing your request, please create an issue on https://github.com/puf17640/git-badges/issues and give as much detail as you can. Thanks!', error });
+const createError = (res, error) => res.status(5e2).send({
+	message: 'An error occured while processing your request, please create an issue on https://github.com/puf17640/git-badges/issues and give as much detail as you can. Thanks!',
+	error
+});
 
-app.use(express.urlencoded({ extended: false }))
-app.set('trust proxy', true)
+app.use(express.urlencoded({ extended: false }));
+app.set('trust proxy', true);
 
 app.get('/visits/:user/:repo', async (req, res) => {
-	const { user, repo } = req.params
+	const { user, repo } = req.params;
 	const response = await request(`https://api.github.com/repos/${user}/${repo}`)
-		.header(githubHeaders).json()
-	if (!response.id) return createError(res, response.message)
-	const flag = req.ip.startsWith(process.env.IPWHITELIST) ? 1 : 0
-	const { counter } = await Entry.findOneAndUpdate({ key: `${user}/${repo}` }, { $inc: { counter: flag } }, { upsert: true, new: true }).exec()
-	if (flag) console.log(`[${user}/${repo}] => ${counter}`)
-	return res.contentType('image/svg+xml')
-		.header('Cache-Control', 'no-cache,max-age=600')
-		.send(await request(`https://img.shields.io/badge/Visits-${counter}-brightgreen${req.originalUrl.slice(req.originalUrl.indexOf('?'))}`).raw())
-})
+		.header(githubHeaders).json();
+	if (!response.id) return createError(res, response.message);
+	const flag = req.ip.startsWith(process.env.IPWHITELIST) ? 1 : 0;
+	const { counter } = await Entry.findOneAndUpdate(
+		{ key: `${user}/${repo}` },
+		{ $inc: { counter: flag } },
+		{ upsert: true, new: true }
+	).exec();
+	if (flag) console.log(`[${user}/${repo}] => ${counter}`);
+	return res.contentType('image/svg+xml').header('Cache-Control', 'no-cache,max-age=600').send(
+		await request(
+			`https://img.shields.io/badge/Visits-${counter}-brightgreen${req.originalUrl.slice(req.originalUrl.indexOf('?'))}`
+		).raw());
+});
 
 app.get('/years/:user', async (req, res) => {
-	const { user } = req.params
+	const { user } = req.params;
 	const response = await request(`https://api.github.com/users/${user}`)
-		.header(githubHeaders).json()
-	if (!response.created_at) return createError(res, response.message)
-	const yearsAtGitHub = differenceInYears(Date.now(), Date.parse(response.created_at))
-	return res.contentType('image/svg+xml')
-		.header('Cache-Control', 'no-cache,max-age=600')
-		.send(await request(`https://img.shields.io/badge/Years-${yearsAtGitHub}-brightgreen${req.originalUrl.slice(req.originalUrl.indexOf('?'))}`).raw())
-})
+		.header(githubHeaders).json();
+	if (!response.created_at) return createError(res, response.message);
+	const yearsAtGitHub = differenceInYears(Date.now(), Date.parse(response.created_at));
+	return res.contentType('image/svg+xml').header('Cache-Control', 'no-cache,max-age=600').send(
+		await request(
+			`https://img.shields.io/badge/Years-${yearsAtGitHub}-brightgreen${req.originalUrl.slice(req.originalUrl.indexOf('?'))}`
+		).raw());
+});
 
 app.get('/repos/:user', async (req, res) => {
-	const { user } = req.params
+	const { user } = req.params;
 	const response = await request(`https://api.github.com/users/${user}`)
-		.header(githubHeaders).json()
-	if (!response.public_repos) return createError(res, response.message)
-	return res.contentType('image/svg+xml')
-		.send(await request(`https://img.shields.io/badge/Repos-${response.public_repos}-brightgreen${req.originalUrl.slice(req.originalUrl.indexOf('?'))}`).raw())
-})
+		.header(githubHeaders).json();
+	if (!response.public_repos) return createError(res, response.message);
+	return res.contentType('image/svg+xml').send(
+		await request(
+			`https://img.shields.io/badge/Repos-${response.public_repos}-brightgreen${req.originalUrl.slice(req.originalUrl.indexOf('?'))}`
+		).raw());
+});
 
 app.get('/gists/:user', async (req, res) => {
 	const { user } = req.params;
 	const response = await request(`https://api.github.com/users/${user}/gists`)
 		.header(githubHeaders).json();
-	if (!Array.isArray(response) || response.length === 0) return createError(res, response.message)
-	return res.contentType('image/svg+xml')
-		.send(await request(`https://img.shields.io/badge/Gists-${response.length}-brightgreen${req.originalUrl.slice(req.originalUrl.indexOf('?'))}`).raw());
-})
+	if (!Array.isArray(response) || response.length === 0) return createError(res, response.message);
+	return res.contentType('image/svg+xml').send(
+		await request(
+			`https://img.shields.io/badge/Gists-${response.length}-brightgreen${req.originalUrl.slice(req.originalUrl.indexOf('?'))}`
+		).raw());
+});
 
 app.get('/updated/:user/:repo', async (req, res) => {
 	const { user, repo } = req.params;
 	const response = await request(`https://api.github.com/repos/${user}/${repo}`)
-		.header(githubHeaders).json()
-	if (!response.id) return createError(res, response.message)
-	return res.contentType('image/svg+xml')
-		.send(await request(`https://img.shields.io/badge/Updated-${moment(response.updated_at).fromNow()}-brightgreen${req.originalUrl.slice(req.originalUrl.indexOf('?'))}`).raw());
-})
+		.header(githubHeaders).json();
+	if (!response.id) return createError(res, response.message);
+	return res.contentType('image/svg+xml').send(
+		await request(
+			`https://img.shields.io/badge/Updated-${moment(response.updated_at).fromNow()}-brightgreen${req.originalUrl.slice(req.originalUrl.indexOf('?'))}`
+		).raw());
+});
 
 app.get('/created/:user/:repo', async (req, res) => {
 	const { user, repo } = req.params;
 	const response = await request(`https://api.github.com/repos/${user}/${repo}`)
-		.header(githubHeaders).json()
-	if (!response.id) return createError(res, response.message)
-	return res.contentType('image/svg+xml')
-		.send(await request(`https://img.shields.io/badge/Created-${moment(response.created_at).fromNow()}-brightgreen${req.originalUrl.slice(req.originalUrl.indexOf('?'))}`).raw());
-})
+		.header(githubHeaders).json();
+	if (!response.id) return createError(res, response.message);
+	return res.contentType('image/svg+xml').send(
+		await request(
+			`https://img.shields.io/badge/Created-${moment(response.created_at).fromNow()}-brightgreen${req.originalUrl.slice(req.originalUrl.indexOf('?'))}`
+		).raw());
+});
 
 app.get('/commits/:periodicity/:user', async (req, res) => {
 	const { periodicity, user } = req.params;
-	if (!Object.keys(periodicities).includes(periodicity)) return res.send(`Please use one of the following periodicities: [${Object.keys(periodicities).join(', ')}]`)
-	const response = await request(`https://api.github.com/search/commits?q=author:${user}+author-date%3A>=${periodicity === 'all' ? '1970-01-01' : moment().startOf(periodicities[periodicity]).format('YYYY-MM-DD')}`)
+	if (!Object.keys(periodicities).includes(periodicity)) { return res.send(`Please use one of the following periodicities: [${Object.keys(periodicities).join(', ')}]`); }
+	const response = await request(
+		`https://api.github.com/search/commits?q=author:${user}+author-date%3A>=${
+			periodicity === 'all'
+				? '1970-01-01'
+				: moment().startOf(periodicities[periodicity]).format('YYYY-MM-DD')}`
+	).header(githubHeaders).json();
+	if (!response.total_count) return createError(res, response.message);
+	return res.contentType('image/svg+xml').send(
+		await request(
+			`https://img.shields.io/badge/${
+				periodicity === 'all'
+					? 'All commits'
+					: periodicity === 'daily'
+						? 'Commits%20today'
+						: `Commits%20this%20${periodicities[periodicity]}`
+			}-${response.total_count}-brightgreen${req.originalUrl.slice(req.originalUrl.indexOf('?'))}`
+		).raw());
+});
+
+app.get('/contributors/:user/:repo', async (req, res) => {
+	const { user, repo } = req.params;
+	let { size = 100, padding = 5 } = req.query;
+	[size, padding] = [parseInt(size), parseInt(padding)];
+	const response = await request(`https://api.github.com/repos/${user}/${repo}/contributors`)
 		.header(githubHeaders).json();
-	if (!response.total_count) return createError(res, response.message)
-	return res.contentType('image/svg+xml')
-		.send(await request(`https://img.shields.io/badge/${periodicity === 'all'
-			? 'All commits' : periodicity === 'daily'
-				? 'Commits%20today' : `Commits%20this%20${periodicities[periodicity]}`}-${response.total_count}-brightgreen${req.originalUrl.slice(req.originalUrl.indexOf('?'))}`).raw());
-})
+	if (!Array.isArray(response)) return createError(res, response.message);
+	return res.contentType('image/svg+xml').send(`
+		<svg role="img" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"> 
+			<defs>
+				${response.map((_c, i) =>
+		`<clipPath id="c-${i}">
+						<circle cx="${((i % 10) * (size + padding)) + (size / 2)}" cy="${(Math.floor(i / 10) * (size + padding)) + (size / 2)}" r="${size / 2}" fill="#000" />
+					</clipPath>`).join('')}
+			</defs>
+			${response.map((contributor, i) =>
+		`<a xlink:href="${contributor.html_url}">
+					<image clip-path="url(#c-${i})" width="${size}" height="${size}" x="${(i % 10) * (size + padding)}" y="${Math.floor(i / 10) * (size + padding)}" xlink:href="${contributor.avatar_url}" />
+				</a>`).join(' ')}
+		</svg>`);
+});
 
-app.use((_, res) => res.redirect('https://pufler.dev/git-badges'))
+app.use((_, res) => res.redirect('https://pufler.dev/git-badges'));
 
-app.listen(process.env.PORT, () => console.log(`[Info]: listening on port ${process.env.PORT}`))
+app.listen(process.env.PORT, () => console.log(`[Info]: listening on port ${process.env.PORT}`));
 
 cron.job('*/10 * * * *', async () => {
-	const { resources } = await request(`https://api.github.com/rate_limit`)
-		.header(githubHeaders).json();
-	console.log(`[Rate Limit]: ${resources.core.remaining} / ${resources.core.limit} (${new Date(resources.core.reset * 1e3).toLocaleString()})`)
-}).start()
+	const { resources } = await request(`https://api.github.com/rate_limit`).header(githubHeaders).json();
+	console.log(`[Rate Limit]: ${resources.core.remaining} / ${resources.core.limit} (${new Date(resources.core.reset * 1e3).toLocaleString()})`);
+}).start();
